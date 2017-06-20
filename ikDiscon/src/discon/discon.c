4236699b12c91206c3056477d0dd97eb13f821f2
Elorza Iker <ielorza@ikerlan.es>
2017-06-20 15:23:49 +0200

    builds

--- ikDiscon/src/discon/discon.c
+++ ikDiscon/src/discon/discon.c
@@ -6,27 +6,26 @@
 void ikDiscon_EXPORT DISCON(float *DATA, int FLAG, const char *INFILE, const char *OUTNAME, char *MESSAGE) {
 	int err;
 	static ikClwindconWTCon con;
-	ikClwindconWTConIn in;
-	ikClwindconWTConOut out;
 		
 	if (NINT(DATA[0]) == 0) {
 		ikClwindconWTConParams param;
 		ikClwindconWTCon_initParams(&param);
 		setParams(&param);
-		ikClwindconWTCon_init(con, &param);
+		ikClwindconWTCon_init(&con, &param);
 	}
 	
-	in.pitchBlade1 = (double) DATA[3]/3.1416*180.0; // rad to deg
-	in.pitchBlade2 = (double) DATA[32]/3.1416*180.0; // rad to deg
-	in.pitchBlade3 = (double) DATA[33]/3.1416*180.0; // rad to deg
-	in.generatorTorque = (double) DATA[21]*1.0e-3; // Nm to kNm
-	in.generatorSpeed = (double) DATA[19]; // rad/s
-	in.maximumPower = (double) DATA[12]*1.0e-3; // W to kW
+	con.in.externalMaximumTorque = 0.0; // kNm
+	con.in.externalMinimumTorque = 0.0; // 0.0
+	con.in.externalMaximumPitch = 90.0; // deg
+	con.in.externalMinimumPitch = 0.0; // deg
+	con.in.generatorSpeed = (double) DATA[19]; // rad/s
+	con.in.maximumPower = (double) DATA[12]*1.0e-3; // W to kW
+	con.in.maximumSpeed = 0.0; // rad/s
 	
-	ikClwindconWTCon_step(con, &in, &out);
+	ikClwindconWTCon_step(&con);
 	
-	DATA[46] = (float) (out.torqueDemand*1.0e3); // kNm to Nm
-	DATA[41] = (float) (out.pitchDemandBlade1/180.0*3.1416); // deg to rad
-	DATA[42] = (float) (out.pitchDemandBlade2/180.0*3.1416); // deg to rad
-	DATA[43] = (float) (out.pitchDemandBlade3/180.0*3.1416); // deg to rad
+	DATA[46] = (float) (con.out.torqueDemand*1.0e3); // kNm to Nm
+	DATA[41] = (float) (con.out.pitchDemandBlade1/180.0*3.1416); // deg to rad
+	DATA[42] = (float) (con.out.pitchDemandBlade2/180.0*3.1416); // deg to rad
+	DATA[43] = (float) (con.out.pitchDemandBlade3/180.0*3.1416); // deg to rad
 }	
